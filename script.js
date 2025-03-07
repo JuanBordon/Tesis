15fc711616063dfd890b5895590146117608871a
florencia.avila <florencia.avila@isep.edu.ar>
2025-03-07 16:18:50 -0300

    java

---
+++ script.js
@@ -0,0 +1,5 @@
+// Función para marcar un paso como completado
+function markCompleted(step) {
+    // Agrega la clase 'completed' para marcar el paso como hecho
+    step.classList.toggle('completed');
+}
